d4a2df062789316d56a2e0e55454b438975199cc
Mohamed-Amin-911 <141561179+Mohamed-Amin-911@users.noreply.github.com>
2023-12-31 21:53:31 +0200

    Add files via upload

---
+++ assignment1.dart
@@ -0,0 +1,31 @@
+main() {
+  //Q1
+  int x = 2;
+  int y = 4;
+  print("x+y=${x + y}\nx*y=${x * y}\nx-y=${x - y}");
+
+  //Q2
+  // int a = 2;
+  // int b = 4;
+  // int c = 5;
+  // int d = 7;
+  // int x = (a * b) - (c * d);
+  // print(x);
+
+  //Q3
+  //int x = 2;
+  //double area = r*r*3.14;
+  //print(area);
+
+  //Q4
+  // int a = 4;
+  // int b = 4;
+  // a >= b ? print("Yes") : print("No");
+
+  //Q5
+  // List<String> name1 = ["Mohamed", "Ahmed"];
+  // List<String> name2 = ["Mahmoud", "Ahmed"];
+  // name1.last == name2.last
+  //     ? print("They are brothers")
+  //     : print("They are not brothers");
+}
